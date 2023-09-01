f861e5bad3a15b436bb86f60564c74253d1219ee
Rohith <rohithkumarbathula99@gmail.com>
2023-09-01 22:36:21 +0530

    initial commit

---
+++ src/app/prediction/[name]/page.tsx
@@ -0,0 +1,21 @@
+const getPredictedNationality = async (name: string) => {
+  const res = await fetch(`https://api.nationalize.io?name=${name}`);
+  return res.json();
+};
+
+type Params = { params: { name: string } };
+
+async function Prediction(obj: Params) {
+  const predictedNationality = getPredictedNationality(obj?.params?.name);
+  const [nationality] = await Promise.all([predictedNationality]);
+
+  return (
+    <div>
+      Name: {obj?.params?.name}
+      <br />
+      Nationality: {nationality?.country[0]?.country_id}
+    </div>
+  );
+}
+
+export default Prediction;
